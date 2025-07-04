53cb3cd16c5b2c294135e3e012cd6a8f61cab90f
Nilima Rout <nrout8249@gmail.com>
2025-07-04 14:22:51 +0530

    configure api calls

---
+++ assignment_expert_frontend/src/services/ApiClient.jsx
@@ -0,0 +1,32 @@
+// src/services/apiClient.js
+import axios from "axios";
+import Cookies from "js-cookie";
+
+const API_URL = "http://52.66.34.20:80";
+
+// Create the axios instance
+const apiClient = axios.create({
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Intercept requests before they are sent to include the token (if it exists)
+apiClient.interceptors.request.use(
+  (config) => {
+   const token = Cookies.get("token") || null // Assuming the token is stored under the key 'token'
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`; // Add token to Authorization header
+    }
+    return config; // Return the modified config
+  },
+  (error) => {
+    return Promise.reject(error); // Handle any errors in request
+  }
+);
+
+export default apiClient;
+
+
+
